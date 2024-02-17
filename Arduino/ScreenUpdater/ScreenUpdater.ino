7ad38549b1d4323a788b81a6855f06727547268f
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-02-17 12:54:55 +0100

    Add bucle iterar numeros en textfilds

--- Arduino/ScreenUpdater/ScreenUpdater.ino
+++ Arduino/ScreenUpdater/ScreenUpdater.ino
@@ -9,9 +9,25 @@ SoftwareSerial nextion(2, 3); // RX, TX
 //Gear
 NexText t6 = NexText(0, 1, "t6");
 //Speed
-NexText Speed = NexText(0, 1, "Speed");
+NexText speedd = NexText(0, 1, "Speed");
 //RPM
-NexText Rpm = NexText(0, 19, "t10");
+NexText rpm = NexText(0, 19, "t10");
+
+NexText t24 = NexText(0, 36, "t24");
+NexText t25 = NexText(0, 37, "t25");
+NexText t14 = NexText(0, 28, "t14");
+NexText t15 = NexText(0, 29, "t15");
+NexText t16 = NexText(0, 30, "t16");
+NexText t17 = NexText(0, 31, "t17");
+NexText t18 = NexText(0, 32, "t18");
+NexText t19 = NexText(0, 33, "t19");
+NexText t22 = NexText(0, 34, "t22");
+NexText t23 = NexText(0, 35, "t23");
+NexText t12 = NexText(0, 26, "t12");
+NexText t13 = NexText(0, 27, "t13");
+NexText t21 = NexText(0, 25, "t21");
+NexText t20 = NexText(0, 24, "t22");
+NexText t29 = NexText(0, 23, "t29");
 
 //Breaks
 //Break 1
@@ -34,7 +50,6 @@ NexProgressBar acePedal = NexProgressBar(0, 5, "acePedal");
 //Brake pedal
 NexProgressBar brakePedal = NexProgressBar(0, 4, "brakePedal");
 
-
 void setup() {
   // Inicialización de la comunicación serial y Nextion
   nextion.begin(9600);
@@ -42,34 +57,29 @@ void setup() {
 }
 
 void loop() {
-  
-  t6.setText("1");
+  int vel = 0;
 
-  Speed.setText("1");
-  Rpm.setText("1");
-  
-  brake1.setText("1");
-  brake2.setText("1");
-  brake3.setText("1");
-  brake4.setText("1");
+  for (int i = 0; i < 100; i++) {
+    String textoConvertido = String(vel);
+    const char* textoChar = textoConvertido.c_str();
 
-  engineTemp.setText("1");
-  voltage.setText("1");
+    // Actualizar solo los elementos que han cambiado
+    speedd.setText(textoChar);
+    rpm.setText(textoChar);
+    t6.setText(textoChar);
+    brake1.setText(textoChar);
+    brake2.setText(textoChar);
+    brake3.setText(textoChar);
+    brake4.setText(textoChar);
+    engineTemp.setText(textoChar);
+    voltage.setText(textoChar);
+    acePedal.setValue(textoChar);
+    brakePedal.setValue(textoChar);
 
-  acePedal.setValue(10);
-  brakePedal.setValue(10);
+    // Actualizar otros elementos si es necesario...
 
-  delay(1500);
+    delay(1);  // Omitir o ajustar según la necesidad
 
-  t6.setText("2");
-
-  Speed.setText("2");
-  Rpm.setText("2");
-  
-  brake1.setText("2");
-  brake2.setText("2");
-  brake3.setText("2");
-  brake4.setText("2");
-
-  delay(1500);
+    vel++;
+  }
 }
